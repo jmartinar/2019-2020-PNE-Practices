cedb5cdbbdab68909a9d6d001024bbbbd4b4e4a8
jmartin <j.martinar.2019@alumnos.urjc.es>
2020-02-04 16:30:21 +0100

    fibonacci sum of n fibonacci numbers

---
+++ Session-02/FIbo-sumN.py
@@ -0,0 +1,22 @@
+def fibon(n):
+    if n < 0:
+        print("Incorrect input")
+        # First Fibonacci number is 0
+    elif n == 1:
+        return 0
+    # Second Fibonacci number is 1
+    elif n == 2:
+        return 1
+    else:
+        return fibon(n - 1) + fibon(n - 2)
+
+n = int(input("Introduce the number of fibonacci terms you wanna sum"))
+
+def fibosum(n):
+    fibosum  = 0
+    for i in range(1, n+1):
+        fibosum = fibon(i) + fibosum
+
+    return fibosum
+
+print("The sum of the first 5 fibo terms is:", fibosum(n))
