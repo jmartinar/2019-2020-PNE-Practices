196b26b991b6ba1bd03506b3bd9046ddff257913
jmartin <j.martinar.2019@alumnos.urjc.es>
2020-02-04 15:53:55 +0100

    exercise 4 sumN with function

--- Session-02/sumN.py
+++ Session-02/sumN.py
@@ -1,16 +1,12 @@
 # -- 1 + 2 + 3 + ....... + 20
 # -- 1 + .............. + 100
 
-res20 = 0
 
-for x in range(1, 21):
-    res20 = res20 + x
+def sumn(n):
+    res = 0
+    for i in range(1, n+1):
+        res = res + i
+    return res
 
-print("Sum of 1-20 is:", res20)
-
-res100 = 0
-
-for x in range(1, 101):
-    res100 = res100 + x
-
-print("Sum of 1-100 is:", res100)
+print("Sum of 1-20 is:", sumn(20))
+print("Sum of 1-100 is:", sumn(100))
