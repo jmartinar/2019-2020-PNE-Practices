a9b5e252177b9999dada5f3a6a9a34c591e09de1
jmartin <j.martinar.2019@alumnos.urjc.es>
2020-02-04 15:35:54 +0100

    final change exercise 3

--- Session-01/count.py
+++ Session-01/count.py
@@ -2,3 +2,4 @@
 
 for i in range(1,21):
     print(i, end="  ")
+    
--- Session-01/sum20.py
+++ Session-01/sum20.py
@@ -1,5 +1,8 @@
+#Session 1. Exercise 3
+
 sum = 0
+
 for x in range(0,20):
     sum = sum + x
 
-print(sum)
+print("TOtal sum is:" ,sum)
