6a2c42102bc8a7e5e01cc52f080b848b8ef82e1d
jmartin <j.martinar.2019@alumnos.urjc.es>
2020-02-05 17:50:59 +0100

    Exercise 3 solved, without functions to change the procedure done in exercise 2

--- Session-03/dna_count_file.py
+++ Session-03/dna_count_file.py
@@ -1,27 +1,24 @@
-
-def total_length(dna_seq):
-    counter = 0
-    for letter in dna_seq:
-        counter += 1
-    return counter
-
-
-def letter_counter(dna_seq):
-    a = 0
-    g = 0
-    t = 0
-    c = 0
-    for letter in dna_seq:
-        if letter == "A":
-            a += 1
-        elif letter == "G":
-            g += 1
-        elif letter == "T":
-            t += 1
-        elif letter == "C":
-            c += 1
-    return ("A: ", a, "C: ", c, "T:", t, "G: ", g)
+counter = 0
+a = 0
+g = 0
+t = 0
+c = 0
 
 with open ("dna.txt", "r") as f:
-    print(total_length(f))
-    print(letter_counter(f))
+    for line in f:
+        for letter in line:
+            counter += 1
+            if letter == "A":
+                 a += 1
+            elif letter == "G":
+                g += 1
+            elif letter == "T":
+                t += 1
+            elif letter == "C":
+                c += 1
+
+print("Total length: ", counter)
+print("A: ", a)
+print("T: ", t)
+print("G: ", g)
+print("C: ", c)
