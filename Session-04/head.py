3fac23af78110109614aed784243babdc7ef7f8c
jmartin <j.martinar.2019@alumnos.urjc.es>
2020-02-11 16:14:11 +0100

    head exercise

--- Session-04/head.py
+++ Session-04/head.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
 # -- Constant with the new of the file to open
-FILENAME = "RNU6_269P.txt.txt"
+FILENAME = "RNU6_269P.txt"
 
 # -- Open and read the file
-file_contents = Path(FILENAME).read_text()
+file_contents = Path(FILENAME).read_text().split("\n")[0]
 
 # -- Print the contents on the console
 print(file_contents)
