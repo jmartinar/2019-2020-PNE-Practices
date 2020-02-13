e467613a196643ffb0e616364b5a19979a19318d
jmartin <j.martinar.2019@alumnos.urjc.es>
2020-02-13 14:47:54 +0100

    FUNCTIONS

--- P0/Seq0.py
+++ P0/Seq0.py
@@ -2,3 +2,15 @@ def seq_ping():
     print("OK")
 
 seq_ping()
+
+
+from pathlib import Path
+
+FILENAME = input("introduce a filename")
+def seq_read_fasta(filename):
+    file_contents = Path(FILENAME).read_text().split("\n")[1:]
+    file_contents_final = "".join(file_contents)
+    print(file_contents_final)
+
+seq_read_fasta(FILENAME)
+
