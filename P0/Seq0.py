992429f844b3516f32efced8d15465b5d9eb0cd0
jmartin <j.martinar.2019@alumnos.urjc.es>
2020-02-18 15:26:56 +0100

    .

--- P0/Seq0.py
+++ P0/Seq0.py
@@ -1,16 +1,47 @@
+import pathlib
+
+
 def seq_ping():
-    print("OK")
-
-seq_ping()
+    print("OK!")
 
 
-from pathlib import Path
-
-FILENAME = input("introduce a filename")
 def seq_read_fasta(filename):
-    file_contents = Path(FILENAME).read_text().split("\n")[1:]
-    file_contents_final = "".join(file_contents)
-    print(file_contents_final)
+    # -- Open and read the file
+    file_contents = pathlib.Path(filename).read_text().split("\n")[1:]
+    body = "".join(file_contents)
+    return body
 
-seq_read_fasta(FILENAME)
+
+def seq_len(seq):
+    return len(seq)
+
+
+def seq_count_base(seq, base):
+    return seq.count(base)
+
+
+def seq_count(seq):
+    bases = ["A", "C", "T", "G"]
+    count_bases = []
+    for base in bases:
+        count_bases.append(seq_count_base(seq, base))
+    dictionary = dict(zip(bases, count_bases))
+    return dictionary
+
+
+def seq_reverse(seq):
+    return seq[::-1]
+
+
+def seq_complement(seq):
+    bases = ["A", "C", "T", "G"]
+    bases_complementary = ["T", "G", "A", "C"]
+    dict_bases_complemenytary = dict(zip(bases, bases_complementary))
+    complementary = ""
+    for i in seq:
+        for base, bases_co in dict_bases_complemenytary.items():
+            if i == base:
+                complementary += bases_co
+
+    return complementary
 
