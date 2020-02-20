f4d099de7d8dcae18c664b8c636e03216b908f46
Javi <j.martinar.2019@alumnos.urjc.es>
2020-02-20 20:57:07 +0100

    EX1 EX2

--- Session-06/Ex2.py
+++ Session-06/Ex2.py
@@ -1,19 +1,19 @@
 class Seq:
     "A class for representing sequence objects"
-    def __init__(self, strbases):
+    def __init__(self, strbases): #define in this function all our object1 properties
 
         for x in strbases:
-            if (x != "A") and (x != "T") and (x != "G") and (x !="C"):
+            if (x != "A") and (x != "T") and (x != "G") and (x !="C"):  #checks if its a vaid seq
                 print("Invalid seq detcted")
                 self.strbases = "ERROR"
                 return
         print("New sequence created!")
         self.strbases = strbases
 
-    def __str__(self):
+    def __str__(self):   #return the string of characters of the object
         return self.strbases
 
-    def len(self):
+    def len(self):    #return the lentgh of characters of the object
         return len(self.strbases)
 
     pass
@@ -21,7 +21,7 @@ class Seq:
 #--Main program
 
 def print_seqs(seq_list):
-    for e in range(len(seq_list)):
-        print("Sequence", e, ": (Length:",  seq_list[e].len(), ")",  seq_list[e])
+    for i in range(len(seq_list)):
+        print("Sequence", i, ": (Length:",  seq_list[i].len(), ")",  seq_list[i])
 
 print_seqs([Seq("ACT"), Seq("GATA"), Seq("CAGATA")])
