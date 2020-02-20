22fb714715fb20106995d17f2377390669ee6cef
Javi <j.martinar.2019@alumnos.urjc.es>
2020-02-20 20:56:04 +0100

    EX1

--- Session-06/Ex1.py
+++ Session-06/Ex1.py
@@ -1,19 +1,19 @@
 class Seq:
     "A class for representing sequence objects"
-    def __init__(self, strbases):
+    def __init__(self, strbases): #define in this function all our object1 properties
 
         for x in strbases:
-            if (x != "A") and (x != "T") and (x != "G") and (x !="C"):
+            if (x != "A") and (x != "T") and (x != "G") and (x !="C"):  #checks if its a vaid seq
                 print("Invalid seq detcted")
                 self.strbases = "ERROR"
                 return
         print("New sequence created!")
         self.strbases = strbases
 
-    def __str__(self):
+    def __str__(self):   #return the string of characters of the object
         return self.strbases
 
-    def len(self):
+    def len(self):    #return the lentgh of characters of the object
         return len(self.strbases)
 
     pass
--- Session-06/Ex2.py
+++ Session-06/Ex2.py
@@ -1,9 +1,27 @@
+class Seq:
+    "A class for representing sequence objects"
+    def __init__(self, strbases):
 
+        for x in strbases:
+            if (x != "A") and (x != "T") and (x != "G") and (x !="C"):
+                print("Invalid seq detcted")
+                self.strbases = "ERROR"
+                return
+        print("New sequence created!")
+        self.strbases = strbases
 
+    def __str__(self):
+        return self.strbases
+
+    def len(self):
+        return len(self.strbases)
+
+    pass
+
+#--Main program
 
 def print_seqs(seq_list):
     for e in range(len(seq_list)):
         print("Sequence", e, ": (Length:",  seq_list[e].len(), ")",  seq_list[e])
-
 
 print_seqs([Seq("ACT"), Seq("GATA"), Seq("CAGATA")])
