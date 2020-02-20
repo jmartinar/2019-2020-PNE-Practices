d9cf5ceefe7590902bc5ae343906bb5c91de43ef
Javi <j.martinar.2019@alumnos.urjc.es>
2020-02-20 21:16:07 +0100

    EX1 EX2 EX3 COMPLETED

--- Session-06/Ex3.py
+++ Session-06/Ex3.py
@@ -24,4 +24,25 @@ def print_seqs(seq_list):       #exercise 2: function to print the seq, its posi
     for i in range(len(seq_list)):
         print("Sequence", i, ": (Length:",  seq_list[i].len(), ")",  seq_list[i])
 
-print_seqs([Seq("ACT"), Seq("GATA"), Seq("CAGATA")])
+
+def generate_seqs(pattern, number):    #exercise 3: function to generate a list with bases that repeats number times
+    my_base = []   #create the list
+    base = ""      #create the base
+    for i in range(1, number+1):   #for from 1 to the number of repetitions chosen
+        base = i * pattern        #the pattern is repeated i times in the list and also in the base creating a pyramid
+        base = Seq(base)          #we apply Seq class properties
+        my_base.append(base)      #append it to the main list
+        base = ""                 #restart the base
+    return(my_base)
+
+
+
+seq_list1 = generate_seqs("A", 3)
+seq_list2 = generate_seqs("AC", 5)
+
+print("List 1:")
+print_seqs(seq_list1)
+
+print()
+print("List 2:")
+print_seqs(seq_list2)
