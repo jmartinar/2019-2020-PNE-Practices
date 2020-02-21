2a0da100b53fefacd3bbe626850be4c56ef4048b
Javi <j.martinar.2019@alumnos.urjc.es>
2020-02-21 21:56:58 +0100

    EX1 P1 COMPLETED

--- P1/Ex1.py
+++ P1/Ex1.py
@@ -1,22 +1,4 @@
-class Seq:
-    "A class for representing sequence objects"
-    def __init__(self, strbases): #define in this function all our object1 properties
-
-        for x in strbases:
-            if (x != "A") and (x != "T") and (x != "G") and (x !="C"):  #checks if its a vaid seq
-                print("Invalid seq detcted")
-                self.strbases = "ERROR"
-                return
-        print("New sequence created!")
-        self.strbases = strbases
-
-    def __str__(self):   #return the string of characters of the object
-        return self.strbases
-
-    def len(self):    #return the lentgh of characters of the object
-        return len(self.strbases)
-
-    pass
+from Seq1 import *
 
 def print_seqs(seq_list):       #exercise 2: function to print the seq, its position and its length
     for i in range(len(seq_list)):
