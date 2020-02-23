942ce36eb49c6532b8e920402bccda91eb767843
Javi <j.martinar.2019@alumnos.urjc.es>
2020-02-23 12:14:05 +0100

    EX1 EX2 EX3 EX4 EX5 EX6 EX7 P1 COMPLETED

---
+++ P1/Ex7.py
@@ -0,0 +1,18 @@
+from Seq1 import Seq
+
+#--Main prog
+print("-----|Practice 1, Exercise 7|-----")
+
+s1 = Seq("")
+s2 = Seq("ACTGA")
+s3 = Seq("Invalid sequence")
+
+print("Sequence", 1, ": (Length:",  s1.len(), ")",  s1)
+print(f"Bases: {s1.count()}")
+print(f"Reverse: {s1.reverse()}")
+
+print("Sequence", 2, ": (Length:",  s2.len(), ")",  s2)
+print(f"Reverse: {s2.reverse()}")
+
+print("Sequence", 3, ": (Length:",  s3.len(), ")",  s3)
+print(f"Reverse: {s3.reverse()}")
