49698e6e953374326bc6688fc2ab86516d160c3c
Javi <j.martinar.2019@alumnos.urjc.es>
2020-02-23 12:05:57 +0100

    EX1 EX2 EX3 EX4 P1 COMPLETED

---
+++ P1/Ex4.py
@@ -0,0 +1,12 @@
+from Seq1 import Seq
+
+#--Main prog
+print("-----|Practice 1, Exercise 3|-----")
+
+s1 = Seq("")
+s2 = Seq("ACTGA")
+s3 = Seq("Invalid sequence")
+
+print("Sequence", 1, ": (Length:",  s1.len(), ")",  s1)
+print("Sequence", 2, ": (Length:",  s2.len(), ")",  s2)
+print("Sequence", 3, ": (Length:",  s3.len(), ")",  s3)
