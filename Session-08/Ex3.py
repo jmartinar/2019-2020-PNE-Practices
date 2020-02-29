d96d460a1626d807f52e82253e7298ca1d932558
Javi <j.martinar.2019@alumnos.urjc.es>
2020-02-29 11:39:32 +0100

    EX1 EX2 EX3 SESSION8 COMPLETED

--- Session-08/Ex3.py
+++ Session-08/Ex3.py
@@ -2,15 +2,15 @@ import socket
 
 # SERVER IP, PORT
 PORT = 8080
-IP = "192.168.124.179"
-
+IP = input("Introduce a valid IP: ")
 while True:
   # -- Ask the user for the message
-
+    message = input("Introduce a message to send to the server: ")
   # -- Create the socket
-
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   # -- Establish the connection to the Server
-
+    s.connect((IP, PORT))
   # -- Send the user message
-
+    s.send(str.encode(message))
   # -- Close the socket
+    s.close()
