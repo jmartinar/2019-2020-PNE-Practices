5dedb1fb0f6acbcc9fe82ac5555b70816aabe7b4
Javi <j.martinar.2019@alumnos.urjc.es>
2020-03-07 12:15:50 +0100

    P3 FINISHED

--- P3/Seq-Server.py
+++ P3/Seq-Server.py
@@ -1,121 +1,101 @@
-import pathlib
+from Seq1 import Seq
+import socket
+import termcolor
 
-class Seq:
-    """"A class for representing sequence objects"""
-    def __init__(self, strbases):
-        if strbases == '':
-            print("NULL Seq created!")
-            self.strbases = "NULL"
-        else:
-            for e in strbases:
-                if e not in ["A", "C", "T", "G"]:
-                    print("INVALID seq")
-                    self.strbases = "ERROR"
-                    return
-            print("New sequence created!")
-            self.strbases = strbases
+# Configure the Server's IP and PORT
+PORT = 8080
+IP = "127.0.0.1"
 
-    def __str__(self):
-        return self.strbases
+seq_list = ["TGTGAACATTCTGCACAGGTCTCTGGCTGCGCCTGGGCGGGTTTCTT", "CAGGAGGGGACTGTCTGTGTTCTCCCTCCCTCCGAGCTCCAGCCTTC",
+            "CTCCCAGCTCCCTGGAGTCTCTCACGTAGAATGTCCTCTCCACCCC", "GAACTCCTGCAGGTTCTGCAGGCCACGGCTGGCCCCCCTCGAAAGT",
+            "CTGCAGGGGGACGCTTGAAAGTTGCTGGAGGAGCCGGGGGGAA"]
 
-    def len(self):
-        for e in self.strbases:
-            if e not in ["A", "C", "T", "G"]:
-                return 0
-        return len(self.strbases)
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def count_base(self, base):
-        count_base = 0
-        if self.strbases == '':
-            return 0
-        else:
-            for e in self.strbases:
-                if e not in ["A", "C", "T", "G"]:
-                    return 0
-                else:
-                    if e in base:
-                        count_base += 1
-            return count_base
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    def count(self):
-        count_A = 0
-        count_C = 0
-        count_T = 0
-        count_G = 0
-        counter_list = []
-        bases = ["A", "C", "T", "G"]
-        if self.strbases == '':
-            counter_list.append(count_A)
-            counter_list.append(count_C)
-            counter_list.append(count_T)
-            counter_list.append(count_G)
-            dict1 = dict(zip(bases, counter_list))
-            return dict1
-        else:
-            for e in self.strbases:
-                if e not in bases:
-                    counter_list.append(count_A)
-                    counter_list.append(count_C)
-                    counter_list.append(count_T)
-                    counter_list.append(count_G)
-                    dict1 = dict(zip(bases, counter_list))
-                    return dict1
-                else:
-                    if e in bases[0]:
-                        count_A += 1
-                    elif e in bases[1]:
-                        count_C += 1
-                    elif e in bases[2]:
-                        count_T += 1
-                    elif e in bases[3]:
-                        count_G += 1
-            counter_list.append(count_A)
-            counter_list.append(count_C)
-            counter_list.append(count_T)
-            counter_list.append(count_G)
-            dict2 = dict(zip(bases, counter_list))
-            return dict2
-    def reverse(self):
-        rev_seq = ''
-        if self.strbases == 'NULL':
-            return self.strbases
-        else:
-            for e in self.strbases[::-1]:
-                if e not in ["A", "C", "T", "G"]:
-                    rev_seq = 'ERROR'
-                    return rev_seq
+# -- Step 2: Bind the socket to server's IP and PORT
+ls.bind((IP, PORT))
 
-                else:
-                    rev_seq += e
-        return (rev_seq)
+# -- Step 3: Configure the socket for listening
+ls.listen()
 
-    def complement(self):
-        comp_seq = ""
-        if self.strbases == 'NULL':
-            return self.strbases
-        else:
-            for e in self.strbases:
-                if e not in ["A", "C", "T", "G"]:
-                    comp_seq = 'ERROR'
-                    return comp_seq
-                else:
-                    if e in "A":
-                        comp_seq += "T"
-                    if e in "T":
-                        comp_seq += "A"
-                    if e in "C":
-                        comp_seq += "G"
-                    if e in "G":
-                        comp_seq += "C"
-            return (comp_seq)
+while True:
+    # -- Waits for a client to connect
+    print("Waiting for Clients to connect")
 
-    def read_fasta(self, filename):
-        file_lines = pathlib.Path(filename).read_text().split("\n")
-        body = (file_lines[1:])
-        final_str = ''.join(body)
-        final_str = Seq(final_str)
-        self.strbases = final_str
-        return (final_str)
+    try:
+        (cs, client_ip_port) = ls.accept()
 
+    # -- Server stopped manually
+    except KeyboardInterrupt:
+        print("Server stopped by the user")
 
-    pass
+        # -- Close the listenning socket
+        ls.close()
+
+        # -- Exit!
+        exit()
+
+    else:
+
+        # -- Read the message from the client
+        # -- The received message is in raw bytes
+        msg_raw = cs.recv(2048)
+
+        # -- We decode it for converting it
+        # -- into a human-redeable string
+        msg = msg_raw.decode()
+        argument_command = msg[msg.find(" ") + 1:] #para sacar la cadena que se solicita
+        response = "ERROR"
+
+    #ping
+    if "PING" in msg:
+        response = "OK!\n"
+
+    #get
+    elif "GET" in msg:
+        response = seq_list[int(argument_command)]
+
+    #info
+    elif "INFO" in msg:
+        seq_info = Seq(argument_command)
+        count_bases_string = ""
+        for base, count in seq_info.count().items():
+            s_base = str(base) + ": " + str(count) + " (" + str(
+                round(count / seq_info.len() * 100, 2)) + "%)" + "\n"
+            count_bases_string += s_base
+
+        response = ("Sequence: " + str(seq_info) + "\n" +
+                    "Total length: " + str(seq_info.len()) + "\n" +
+                    count_bases_string)
+
+    #complement
+    elif "COMP" in msg:
+        seq_comp = Seq(argument_command)
+        response = seq_comp.complement() + "\n"
+
+    #reverse
+    elif "REV" in msg:
+        seq_rev = Seq(argument_command)
+        response = seq_rev.reverse() + "\n"
+
+    #gene
+    elif "GENE" in msg:
+        gene = argument_command
+        s = Seq()
+        s.read_fasta("../Session-04/" + gene + ".txt")
+        response = str(s) + "\n"
+
+        # -- The message has to be encoded into bytes
+        # Server Console
+
+    termcolor.cprint(msg[:msg.find(" ")], "green")
+    print(response)
+
+    # Client console
+    cs.send(response.encode())
+    # -- Close the data socket
+    cs.close()
